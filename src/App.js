66bc2a323a58292e0e533cde6789ad0678cfee08
yujin-0 <yujinjin0122@gmail.com>
2025-02-11 13:53:48 +0900

    0211

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Mainpage from "./Mainpage";
+import FreeBoard from "./Freeboard";
+import CreatePost from "./CreatePost";
+import PostDetail from "./PostDetail";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route index element={<Mainpage />} />
-        </Routes>
+        <Route path="/" element={<FreeBoard />} />
+        <Route path="/create" element={<CreatePost />} />
+        <Route path="/post/:postId" element={<PostDetail />} />
+      </Routes>
     </Router>
   );
 }
