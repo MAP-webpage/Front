c6c948d4125b3184e1649da5b4f333861cdcf3d8
eggofgrape <seobin0007@naver.com>
2025-02-20 01:43:00 +0900

    캘린더랑 이미지 업로더

---
+++ src/Calendar/Calendarpage.js
@@ -0,0 +1,12 @@
+import { useEffect, useState } from "react";
+import MyCalendar from "./MyCalendar";
+
+function Calendarpage() {
+  return (
+    <div>
+      <h1>Calendar 메인 페이지</h1>
+      <MyCalendar/>
+    </div>
+  );
+}
+export default Calendarpage;
