8d7e188e38d8c8da3768ecf225b3be4dac1af4e4
xxanecdote@naver.com <xxanecdote@naver.com>
2025-02-21 18:28:42 +0900

    비밀밀번호 변경모달 임시구현

--- src/MyPage/MyPage.jsx
+++ src/MyPage/MyPage.jsx
@@ -1,7 +1,7 @@
 import './MyPage.css'
-import LogoutButton from "./components/LogoutButton";  // ✅ 경로 확인
-import ProfileDetails from "./components/ProfileDetails";  // ✅ 경로 확인
-import ProfileHeader from "./components/ProfileHeader";  // ✅ 경로 확인
+import LogoutButton from "./components/LogoutButton"; 
+import ProfileDetails from "./components/ProfileDetails"; 
+import ProfileHeader from "./components/ProfileHeader"; 
 import projectMapLogo from "./image/project_maplogo.png";
 
 // props 미구현, App.jsx 에서 받아와야함
